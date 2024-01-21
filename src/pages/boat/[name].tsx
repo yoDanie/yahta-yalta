6f24c9b59100bae68e4f3382876c39e48dd6d37a
Daniil Ilyin <d.v.ilyin@tinkoff.ru>
2024-01-21 16:17:15 +0300

    wip stuff

--- src/pages/boat/[name].tsx
+++ src/pages/boat/[name].tsx
@@ -1,8 +1,25 @@
+import { boatsData } from 'data'
 import { useRouter } from 'next/router'
+
+import styles from './index.module.scss'
 
 export const BoatPage = () => {
   const router = useRouter()
-  return <div style={{}}>{router.query.slug}</div>
+  const boatName = router.query.name
+
+  const boatData = boatsData.find(({ name }) => name === boatName)
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.header}>
+        <div className={styles.photo}>main + 3 photos</div>
+        <div className={styles.info}>
+          <div className={styles.title}>{boatName}</div>
+          <div className={styles.specs}>main specs</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default BoatPage
